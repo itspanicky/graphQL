7cea60075f68b7dd8efbb40f447aeed9c86e6a3c
itspanicky <nicky.li92@gmail.com>
2020-06-28 21:29:48 -0400

    feat: user and post model setup

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 
 app.get("/", (req, res) => res.send("Hello World"));
+app.use(bodyParser.json());
 
 app.listen(5000, () => console.log('Server is running on port 5000'));
