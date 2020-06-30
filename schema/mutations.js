eca89aff590cbf5bf7054defdbfebcf95f3d9650
itspanicky <nicky.li92@gmail.com>
2020-06-29 23:40:25 -0400

    feat: mutation to create new post

--- schema/mutations.js
+++ schema/mutations.js
@@ -4,24 +4,37 @@ const mongoose = require("mongoose");
 const { GraphQLObjectType, GraphQLString, GraphQLNonNull } = graphql;
 const UserType = require("./user_type");
 const User = mongoose.model("user");
+const PostType = require("./post_type");
+const Post = mongoose.model("post");
 
 // Mutation that will allow us to add users using GraphiQL
 const mutation = new GraphQLObjectType({
   name: "Mutation",
   fields: {
-      // name of mutation
-      newUser: {
-          type: UserType,
-          args: {
-              name: { type: new GraphQLNonNull(GraphQLString) },
-              email: { type: new GraphQLNonNull(GraphQLString) },
-              password: { type: new GraphQLNonNull(GraphQLString) }
-          },
-          resolve(parentValue, { name, email, password }) {
-              return new User({ name, email, password }).save();
-          }
-      }
-  }
+    // name of mutation
+    newUser: {
+      type: UserType,
+      args: {
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        email: { type: new GraphQLNonNull(GraphQLString) },
+        password: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve(parentValue, { name, email, password }) {
+        return new User({ name, email, password }).save();
+      },
+    },
+    newPost: {
+      type: PostType,
+      args: {
+        title: { type: new GraphQLNonNull(GraphQLString) },
+        body: { type: new GraphQLNonNull(GraphQLString) },
+        author: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve(parentValue, { title, body, author }) {
+        return new Post({ title, body, author }).save();
+      },
+    },
+  },
 });
 
 module.exports = mutation;
